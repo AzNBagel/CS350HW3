8d2eb63392f8fdfe51b7822f9ed35f8e2603c7ce
Andrew <andrewmccann88@gmail.com>
2016-02-23 21:19:00 -0800

    Initial commit

---
+++ goldPath.py
@@ -0,0 +1,26 @@
+
+
+
+""""
+Andrew McCann
+CS350
+Assignment #3
+
+Given a table composed of N x M
+cells, each containing some number of
+gold coins.  You start from the upper left hand corner and at each step may
+go down or to the right.  At each step you collect all the coins that are in
+that location.  Find the maximum number of coins you can collect
+
+"""""
+
+# Input: N by M matrix
+# Output: Maximum sum of non-zero values within the matrix in a left-right path only.
+
+def gold(matrix):
+    maxGold = matrix[0,0]
+
+    for i in range()
+
+
+    return maxGold
